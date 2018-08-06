bb977d373f6bdf1090912c18d702279dbbc1988a
johnjayasingh <johnjayasingh.s@gmail.com>
2018-08-06 22:11:24 +0530

    hash function added

--- client/lib/helper.js
+++ client/lib/helper.js
@@ -2,4 +2,5 @@ const { createHash } = require('crypto');
 
 module.exports = {
     leafHash: (input, length) => createHash('sha512').update(input).digest('hex').toLowerCase().slice(0, length),
+    hash: (input, length) => createHash('sha512').update(input).digest('hex').toLowerCase().slice(0, length),
 }
