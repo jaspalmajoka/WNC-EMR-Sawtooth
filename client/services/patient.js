69cbc9c18ff2b92e83cfe4f40328cd2e6e169bf3
johnjayasingh <johnjayasingh.s@gmail.com>
2018-08-06 23:02:17 +0530

    error response updated

--- client/services/patient.js
+++ client/services/patient.js
@@ -7,13 +7,19 @@ const sawtoothWalletClient = new SawtoothWalletClient(client);
 
 module.exports = {
     createPatient: (req, res) => {
-        sawtoothWalletClient.submit(req)
-            .then((data) => {
-                res.json({ success: true, data }).end();
-            })
-            .catch((err) => {
-                res.status(400).json({ success: true, err }).end();
-            });
+        const payload = req.body;
+        const { Action, Data } = payload;
+        if (Action && Data) {
+            sawtoothWalletClient.submit(payload)
+                .then((data) => {
+                    res.json({ success: true, data }).end();
+                })
+                .catch((err) => {
+                    res.status(400).json({ success: true, err }).end();
+                });
+        } else {
+            res.status(400).json({ success: true, nessage: 'Payload Error' }).end();
+        }
     },
     getPatient: (req, res) => {
         res.json({ success: true, message: 'TODO Get Patient' })
