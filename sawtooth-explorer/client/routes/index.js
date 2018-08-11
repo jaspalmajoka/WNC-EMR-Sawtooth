cfa5082b795cc3d39cc94fc44cd025901a22b18f
johnjayasingh <johnjayasingh.s@gmail.com>
2018-08-11 22:00:34 +0530

    heh

--- sawtooth-explorer/client/routes/index.js
+++ sawtooth-explorer/client/routes/index.js
@@ -3,11 +3,8 @@ const patient = require('./../services/patient');
 const document = require('./../services/document');
 
 router.get('/patient', patient.getPatient);
-
 router.post('/patient', patient.createPatient);
-
 router.put('/patient/:id', patient.updatePatient);
-
 router.delete('/patient/:id', patient.deletePatient);
 
 router.post('/document', document.addDocumet);
