2f22743ded0c56ce51266a08afff9835158bce08
johnjayasingh <johnjayasingh.s@gmail.com>
2018-08-11 15:59:56 +0530

    public endpoint

--- sawtooth-explorer/src/environments/environment.ts
+++ sawtooth-explorer/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiURL: 'http://13.251.142.80/sawtooth',
+  apiURL: process.env.PUBLIC_RESTAPI || 'http://13.251.142.80/sawtooth',
   apiTimeout: 3000
 };
