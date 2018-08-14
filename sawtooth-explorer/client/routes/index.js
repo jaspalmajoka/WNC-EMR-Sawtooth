236808c5df8ec01ef83e7b3a77dd8af053886bf8
johnjayasingh <johnjayasingh.s@gmail.com>
2018-08-14 19:55:03 +0530

    routes and swagger update

--- sawtooth-explorer/client/routes/index.js
+++ sawtooth-explorer/client/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const patient = require('./../services/patient');
 const document = require('./../services/document');
+const hospital = require('./../services/hospital');
 
 router.get('/patient', patient.getPatient);
 router.post('/patient', patient.createPatient);
@@ -11,6 +12,8 @@ router.post('/document', document.addDocument);
 router.delete('/document/:id', document.deleteDocument);
 router.get('/document', document.getDocument);
 
+router.post('/hospital', hospital.addHospital);
+router.get('/hospital', hospital.getHospital);
 
 
 module.exports = router;
