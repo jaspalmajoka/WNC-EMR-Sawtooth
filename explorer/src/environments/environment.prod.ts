2f5f2aa4e8f6ff6581b89764ab8bce2a775edd5f
johnjayasingh <johnjayasingh.s@gmail.com>
2018-08-15 22:19:21 +0530

    fix

--- explorer/src/environments/environment.prod.ts
+++ explorer/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'http://18.136.137.49:8090',
+  namespace: {
+    '10': 'patient',
+    '11': 'document',
+    '20': 'hospital',
+  }
 };
