75e230047afa8d592e14ac0497387af469637f32
johnjayasingh <johnjayasingh.s@gmail.com>
2018-08-15 21:36:20 +0530

    remo

--- sawtooth-explorer/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiURL: 'http://18.136.137.49:8090',
-  apiTimeout: 3000
-};
--- sawtooth-explorer/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiURL: 'http://18.136.137.49:8090',
-  apiTimeout: 3000
-};
