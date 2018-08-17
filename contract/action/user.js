49d18e06e57752d9cab40ac73e60fe21458d875a
johnjayasingh <johnjayasingh.s@gmail.com>
2018-08-17 22:13:37 +0530

    fix

--- contract/action/user.js
+++ contract/action/user.js
@@ -14,8 +14,32 @@ module.exports = {
         data
     }) => {
         const {
-            id
+            id,
+            timestamp
         } = data;
+        const userAddress = _createUserAddress(id);
+        const possibleAddressValues = await context.getState([userAddress]).catch(toInvalidTransaction);
+
+        const userValuesRep = possibleAddressValues[userAddress];
+
+        let stateValue = {};
+        if (!userValuesRep || userValuesRep.length === 0) {
+            return toInvalidTransaction('User ID is not registered');
+        }
+        if (!timestamp) {
+            return toInvalidTransaction('Timestamp is not supplied');
+        }
+        stateValue = JSON.parse(userValuesRep);
+        if (!stateValue) {
+            return toInvalidTransaction('State doesn"t contain values');
+        }
+        if (!stateValue.logins) {
+            stateValue.logins = [];
+        }
+        stateValue.logins.push(timestamp);
+        // If any data is supplied otherthan id and timestamp its appended
+        Object.assign(stateValue, data);
+        return setEntry(context, userAddress, stateValue);
     },
     userRegister: async ({
         context,
@@ -24,6 +48,21 @@ module.exports = {
         const {
             id
         } = data;
+        const userAddress = _createUserAddress(id);
+        const possibleAddressValues = await context.getState([userAddress]).catch(toInvalidTransaction);
+
+        const userValuesRep = possibleAddressValues[userAddress];
+
+        let stateValue;
+        if (userValuesRep && userValuesRep.length) {
+            stateValue = JSON.parse(userValuesRep);
+            if (stateValue) {
+                return toInvalidTransaction('User ID already registered');
+            }
+        }
+
+        stateValue = data;
+        return setEntry(context, userAddress, stateValue);
     },
     userUpdate: async ({
         context,
