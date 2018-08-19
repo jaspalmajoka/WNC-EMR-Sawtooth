9c4b988c74e8fce7c3f96a634d8fb8e382082116
johnjayasingh <johnjayasingh.s@gmail.com>
2018-08-19 18:02:52 +0530

    localconfig added

---
+++ explorer/src/environments/environment.local.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  apiURL: 'http://localhost:8090',
+  namespace: {
+    '10': 'patient',
+    '11': 'document',
+    '20': 'hospital',
+    '30': 'user',
+    '00': 'settings',
+  }
+};
